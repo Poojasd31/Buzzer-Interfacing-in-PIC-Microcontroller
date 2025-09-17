18fc19ea46283fa7d7f9fcd746791d9c27e8a280
POOJA DHAKANE <119505464+Poojasd31@users.noreply.github.com>
2025-09-17 17:40:23 +0530

    Add files via upload

---
+++ Buzzer_Sw.c
@@ -0,0 +1,50 @@
+
+// PIC16F877A Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+
+
+#include <xc.h>
+#include <pic16f877a.h>
+#define _XTAL_FREQ 20000000
+
+#define SW3 RB5
+#define Buzzer RE0
+
+void main(void) 
+{
+    TRISB5 = 1; // switch - input
+    TRISE0 = 0; // buzzer - output
+   
+    ADCON1 = 0x07; // Configure all AN pins as digital I/O
+    
+    SW3 = 1; //OFF
+    Buzzer = 0; //OFF
+    while (1)
+    {
+        if (SW3 == 0)
+        {
+            __delay_ms(50); // debounce
+            Buzzer = 1;
+        }
+        else
+        {
+            Buzzer = 0;
+        }
+    }
+}
